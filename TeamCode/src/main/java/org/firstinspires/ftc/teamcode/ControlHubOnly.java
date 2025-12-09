2865437010dec71629c1a2405c2e6080959e771b
Roman <romanfox242@gmail.com>
2025-12-08 17:20:44 -0800

    Add ControlHubOnly and FlywheelOnly teleop modes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ControlHubOnly.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp
+public class ControlHubOnly extends OpMode {
+    // Declare OpMode members.
+    private final ElapsedTime runtime = new ElapsedTime();
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initializing");
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit START
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits START
+     */
+    @Override
+    public void start() {
+        telemetry.addData("Status", "Start requested");
+        runtime.reset();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits START but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        // Show the elapsed game time.
+        telemetry.addData("Status", "Run Time: " + runtime);
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+        telemetry.addData("Status", "Stop requested");
+    }
+}
