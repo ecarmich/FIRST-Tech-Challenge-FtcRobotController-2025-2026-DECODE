87ea111375ac051da7beef0f3dbbd3929043f52c
Eric Carmichael <ecarmich@alumni.rice.edu>
2026-01-19 09:10:54 -0800

    Add empty PinpointDriftKalmanFilter class and a JUnit test class, to verify the tests are set up correctly

---
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/PinpointDriftKalmanFilterTest.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PinpointDriftKalmanFilterTest {
+    @Test
+    public void a() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void b() {
+    }
+}
